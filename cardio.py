f9ce9e717532677f3b0894dee14e990d9a5be797
Roger Esteve <rogeresteve30@gmail.com>
2023-12-28 17:02:17 +0100

    different methods, same result

--- cardio.py
+++ cardio.py
@@ -6,7 +6,11 @@ from tqdm import trange
 from torch import nn, Tensor
 import torch
 
-def transform_dataset(dataset):
+def transform_dataset(dataset, test=False):
+    if test:
+        dataset = dataset[["age", "height", "weight", "ap_hi", "ap_lo", "gender", "smoke", "alco", "active", "cholesterol", "gluc"]]
+    else:
+        dataset = dataset[["age", "height", "weight", "ap_hi", "ap_lo", "gender", "smoke", "alco", "active", "cholesterol", "gluc", "cardio"]]
     dataset["age"] = dataset["age"] / max(dataset["age"])
     dataset["height"] = dataset["height"] / max(dataset["height"])
     dataset["weight"] = dataset["weight"] / max(dataset["weight"])
@@ -21,8 +25,9 @@ def load_dataset():
     test = pd.read_csv("dataset/test.csv")
 
     train = transform_dataset(train)
+    Y_train = train["cardio"].values.reshape(-1, 1)
 
-    return train.drop(columns="cardio").values, train["cardio"].values.reshape(-1, 1), transform_dataset(test).values, test.values
+    return train.drop(columns="cardio").values, Y_train, transform_dataset(test, test=True).values, test.values
 
 def save_result(result, model):
     submission = pd.read_csv('dataset/sample.csv')
@@ -34,21 +39,51 @@ def save_result(result, model):
     submission.to_csv(f'submission_{i}.csv', index = False)
     torch.save(model.state_dict(), f"cardio_{i}.pth")
 
+class BinaryBranch(nn.Module):
+    def __init__(self):
+        super(BinaryBranch, self).__init__()
+        self.seq = nn.Sequential(
+            nn.Linear(10, 32),
+            nn.ReLU()
+        )
+
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.seq(x)
+
+class DecimalBranch(nn.Module):
+    def __init__(self):
+        super(DecimalBranch, self).__init__()
+        self.seq = nn.Sequential(
+            nn.Linear(5, 32),
+            nn.ReLU(),
+            nn.BatchNorm1d(32)
+        )
+
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.seq(x)
+
+
 class CardioNet(nn.Module):
     def __init__(self):
         super(CardioNet, self).__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(15, 64),
-            nn.ReLU(),
+        self.binary_branch = BinaryBranch()
+        self.decimal_branch = DecimalBranch()
+        self.final = nn.Sequential(
             nn.Linear(64, 1),
             nn.Sigmoid()
         )
     
     def __call__(self, x: Tensor) -> Tensor:
-        return self.seq(x)
+        x = x.reshape(-1, 15)
+        x_dec = x[:, :5]
+        x_bin = x[:, 5:]
 
-    def parameters(self):
-        return self.seq.parameters()
+        x_bin = self.binary_branch(x_bin)
+        x_dec = self.decimal_branch(x_dec)
+
+        x = torch.cat((x_bin, x_dec), dim=1)
+        return self.final(x)
+
 
 
 if __name__ == "__main__":
@@ -60,29 +95,35 @@ if __name__ == "__main__":
     # train
     BS = 64
     optim = torch.optim.Adam(model.parameters(), lr=0.001)
-    lr_schedule = torch.optim.lr_scheduler.StepLR(optim, step_size=1000, gamma=0.01)
+    lr_schedule = torch.optim.lr_scheduler.StepLR(optim, step_size=400, gamma=0.002)
     loss_function = nn.BCELoss()
 
     losses = []
     acc = []
+    acc_sum = 0
 
-    for epoch in (t:= trange(10000)):
-        samp = np.random.randint(0, len(X_train), BS)
-        X = torch.tensor(X_train[samp], device="cuda:0").float()
-        Y = torch.tensor(Y_train[samp], device="cuda:0").float()
+    for epoch in range(3):
+        acc_sum = 0
+        for i in (t := trange(0, X_train.shape[0], BS)):
+            samp = np.random.randint(0, len(X_train), BS)
 
-        optim.zero_grad()
-        out = model(X).to("cuda:0")
-        loss = loss_function(out, Y)
-        loss.backward()
-        optim.step()
+            X = torch.tensor(X_train[samp], device="cuda:0").float()
+            Y = torch.tensor(Y_train[samp], device="cuda:0").float()
 
-        lr_schedule.step()
+            optim.zero_grad()
+            out = model(X).to("cuda:0")
+            loss = loss_function(out, Y)
+            loss.backward()
+            optim.step()
 
-        losses.append(loss.item())
-        acc.append((out.round() == Y).sum().item() / BS)
+            lr_schedule.step()
 
-        t.set_description(f"Loss: {loss.item():.2f} - Acc: {acc[-1]:.2f}")
+            losses.append(loss.item())
+            acc.append((out.round() == Y).sum().item() / BS)
+            acc_sum += acc[-1]
+
+            t.set_description(f"Epoch {epoch+1} => Loss: {loss.item():.2f} - Acc: {acc[-1]:.2f}")
+        print(f"Avg acc: {acc_sum / (X_train.shape[0]/BS):.2f}")
 
     
 
@@ -94,7 +135,7 @@ if __name__ == "__main__":
         X = torch.tensor(X_train[i], device="cuda:0").float()
         Y = torch.tensor(Y_train[i], device="cuda:0").float()
 
-        out = model(X)
+        out = model(X).to("cuda:0")
         acc_sum += (out.round() == Y).sum().item()
     print(f"Acc: {acc_sum / X_train.shape[0]:.2f}")
 
